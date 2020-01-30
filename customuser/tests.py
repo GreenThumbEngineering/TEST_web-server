8ae9780dc29a6c0cfbd995b06c443704ff83db72
Filippo Vimini <filippo.vimini@ericsson.com>
2020-01-30 16:41:23 +0200

    updated test file

--- customuser/tests.py
+++ customuser/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.contrib.auth.models import BaseUserManager
+from customuser.models import UserManager
 
-# Create your tests here.
+class UserManagerTestCase(TestCase):
+    
+    def userCreation(self):
+        userEmail = "user@email.com"
+        userPassword = "1234"
+        userId = "123abc"
+        goodUser = UserManager._create_user(BaseUserManager, userId, userEmail, userPassword, False, False)
+        self.assertEqual(goodUser.email, userEmail)
+
