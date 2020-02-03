c29efa1b0233eb5b23705c5ef94f6ab9969f9254
Samuli Oikarinen <oikaris1@aalto.fi>
2020-02-04 01:54:22 +0200

    UI improvements, graph reworks, update plants nickname

--- polls/models.py
+++ polls/models.py
@@ -13,9 +13,10 @@ class PlantData(models.Model):
   Luminosity = models.IntegerField(default=0)
 
 class Plants(models.Model):
-  userid = models.IntegerField()
-  nickname = models.CharField(max_length=30)
-  deviceid = models.CharField(max_length=30)
+  user = models.ForeignKey(custom_user,on_delete=models.CASCADE)
+  nickname = models.CharField(max_length=20, blank=True)
+  deviceid = models.CharField(max_length=20)
+  plant_pic = models.ImageField(upload_to='plant_pics', blank=True)
   
 class UserProfileInfo(models.Model):
   user = models.OneToOneField(custom_user, on_delete=models.CASCADE)
